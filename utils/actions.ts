cb047a9a00ecce46a498e0e4ce181f12b46808ee
Jari Leminaho <jaripiilola@gmail.com>
2024-09-20 20:07:32 +0200

    adds search and product details and more

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,4 +1,5 @@
 import db from '@/utils/db';
+import { redirect } from 'next/navigation';
 
 //! One way to fetch
 export const fetchFeaturedProducts = async () => {
@@ -11,10 +12,31 @@ export const fetchFeaturedProducts = async () => {
 };
 
 //! Alternative way to fetch without async/await
-export const fetchAllProducts = () => {
+export const fetchAllProducts = ({ search = '' }: { search: string }) => {
   return db.product.findMany({
+    where: {
+      OR: [
+        {
+          name: { contains: search, mode: 'insensitive' },
+        },
+        {
+          company: { contains: search, mode: 'insensitive' },
+        },
+      ],
+    },
     orderBy: {
       createdAt: 'desc',
     },
   });
+};
+
+export const fetchSingleProduct = async (productId: string) => {
+  const product = await db.product.findUnique({
+    where: {
+      id: productId,
+    },
+  });
+
+  if (!product) redirect('/products');
+  return product;
 };
