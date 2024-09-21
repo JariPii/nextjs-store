b0e6445371140eb9d8ee1a48f0546991be257ab1
Jari Leminaho <jaripiilola@gmail.com>
2024-09-21 13:18:35 +0200

    adds auth provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from '@/components/navbar/Navbar';
 import Container from '@/components/global/Container';
 import Providers from './providers';
 import { ReactNode } from 'react';
+import { ClerkProvider } from '@clerk/nextjs';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -28,15 +29,17 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Providers>
-          <Navbar />
-          <Container className='py-20'>{children}</Container>
-        </Providers>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang='en' suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Providers>
+            <Navbar />
+            <Container className='py-20'>{children}</Container>
+          </Providers>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,4 +1,21 @@
-const UserIcon = () => {
-  return <div>UserIcon</div>;
+import { LuUser2 } from 'react-icons/lu';
+import { currentUser } from '@clerk/nextjs/server';
+
+const UserIcon = async () => {
+  const user = await currentUser();
+
+  const profileImage = user?.imageUrl;
+
+  if (profileImage) {
+    return (
+      <img
+        src={profileImage}
+        alt='user avatar'
+        className='w-6 h-6 rounded-full object-cover'
+      />
+    );
+  }
+
+  return <LuUser2 className='w-6 h-6 rounded-full object-cover' />;
 };
 export default UserIcon;
